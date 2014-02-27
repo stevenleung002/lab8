cb5a37a69a4a96e63a0e86a7f0aa0fd59cc20369
vagrant <vagrant@precise32.(none)>
2014-02-27 08:07:21 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+
+	$(".likeBtn").click(function(){
+		//alert('button clicked');
+		ga("send", "event", "Category", "Action");
+	});
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	//var data = { 'showGrid': false};
+	projects.showGrid = false;
   	res.render('index', projects);
+};
+
+exports.viewGrid = function(req,res){
+	//var data = { 'showGrid':true};
+	projects.showGrid = true;
+	res.render('index', projects);
 };
